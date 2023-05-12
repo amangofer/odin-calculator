5868afbcfe614959083ab908f9c44cf677eaef76
Amanuel Haile <amanuelhaile369@gmail.com>
2023-05-12 11:33:28 +0300

    create math operator selector function

--- main.js
+++ main.js
@@ -18,3 +18,21 @@ function divide(num1, num2){
 function moduloOpp(num){
     return num / 100;
 }
+
+// operator selector
+function operate(opp, num1, num2){
+
+    switch(opp){
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        case '%':
+            return moduloOpp(num1);
+    }
+
+}
